df54e6e27bae270b35cc46842facad24f38cb719
djyoda <djyoda@gmail.com>
2014-11-23 16:12:55 +0400

    delete backup implemented

--- delete.py
+++ delete.py
@@ -1,64 +1,37 @@
 #!/usr/bin/env python
 
-import os
 import sys
-import logging
-import config_ini
-
-import time
-
-from restore import Restore
-
-from cinderclient.client import Client as CinderClient
-
-username = os.environ['OS_USERNAME']
-password = os.environ['OS_PASSWORD']
-tenant_id = os.environ['OS_TENANT_NAME']
-auth_url = os.environ['OS_AUTH_URL']
-
-cinder_client = CinderClient(1, username, password, tenant_id, auth_url)
-
-appLogger = logging.getLogger(__name__)
+import utils
 
 
 #########################################################################
 class Delete(object):
     """Class for deleting backup"""
 
-    # -------------------------------------------------------------------
+    #--------------------------------------------------------------------
     def __init__(self, backups):
         """
         Constructor
 
-        :param backups: List of backup IDs
+        :param backups: list of backup id's
         :type backups: list
         """
         self.backups = backups
 
-    # -------------------------------------------------------------------
-    def delete_backup(self, backup_id):
+    #--------------------------------------------------------------------
+    def do_delete_backup(self):
         """
-        :param backup_id: Backup ID
-        :type backup_id: str
+        Delete volume backup
         """
-        status = Restore.get_backup_status(backup_id)
-        if status == "available":
-            cinder_client.backups.delete(backup_id)
-            appLogger.info("Deleting backup: %s" % backup_id)
-        else:
-            appLogger.error("Unable to delete backup: %s. The backup status is %s"
-                            % (backup_id, status))
+
+        for backup_id in self.backups:
+            utils.delete_backup(backup_id)
 
 
 def main():
-    # Provide list of backup IDs
     backups = sys.argv[1:]
-    # Start deleting backups
-    appLogger.info("Backup deletion started.")
     bckp_del = Delete(backups)
-    for bckp_id in backups:
-        bckp_del.delete_backup(bckp_id)
-    appLogger.info("Backup deletion finished")
+    bckp_del.do_delete_backup()
 
 if __name__ == "__main__":
     main()
